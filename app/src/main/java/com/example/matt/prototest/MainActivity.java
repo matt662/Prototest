613730d6d1e0aa5f4b2fa1fc839ce0640fcd1234
Matt F <matthewf662@gmail.com>
2015-01-21 15:43:35 -0500

    Working find stop times from stop id (not code), still need to add pull info from internet and be able to put in stop code

--- app/src/main/java/com/example/matt/prototest/MainActivity.java
+++ app/src/main/java/com/example/matt/prototest/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.matt.prototest;
 
+import android.content.res.AssetManager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -7,12 +8,18 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.google.transit.realtime.GtfsRealtime.*;
 import com.google.protobuf.*;
 
 import java.io.*;
 import java.net.URL;
+import java.net.URLConnection;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -23,28 +30,43 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
         Button GObutton = (Button)findViewById(R.id.goButton);
         final EditText stopIDEdit = (EditText)findViewById(R.id.editText);
+        final TextView timesView = (TextView)findViewById(R.id.stopTimesText);
 
         GObutton.setOnClickListener(
                 new Button.OnClickListener() {
                     public void onClick(View v) {
                         try {
-                            URL url = new URL("http://opendata.hamilton.ca/GTFS-RT/GTFS_TripUpdates.pb");
-                            InputStream is = url.openStream();
+                            //URL url = new URL("http://opendata.hamilton.ca/GTFS-RT/GTFS_TripUpdates.pb");
+                            //URLConnection urlConnection = url.openConnection();
+                            //System.out.println("Opened Connection");
+                            //InputStream is = new BufferedInputStream(url.openStream());
+                            //System.out.println("Created url");
+                            AssetManager assetManager = getAssets();
+                            InputStream is = assetManager.open("GTFS_TripUpdates.pb");
                             FeedMessage realdata = FeedMessage.parseFrom(is);
+                            System.out.println("parsed data");
                             String times="";
                             for (FeedEntity entity : realdata.getEntityList()){
                                 if(!entity.hasTripUpdate())continue;
                                 TripUpdate trip = entity.getTripUpdate();
                                 if (!trip.hasTrip())continue;
-                                TripUpdate.StopTimeUpdate stopTime = trip.getStopTimeUpdate();//TODO find out which id to use for getStopTimeUpdate
-                                if (stopTime.getStopId().equals(stopIDEdit.getText().toString())){
-                                    TripUpdate.StopTimeEvent stopEventArrival = stopTime.getArrival();
-                                    times = times +", " + String.valueOf(stopEventArrival.getTime());
+                                for(TripUpdate.StopTimeUpdate stopTime : trip.getStopTimeUpdateList() ) {
+                                    if (stopTime.getStopId().equals(stopIDEdit.getText().toString())) {
+                                        TripUpdate.StopTimeEvent stopEventArrival = stopTime.getArrival();
+                                        long unixSeconds = stopEventArrival.getTime();
+                                        Date date = new Date(unixSeconds*1000);
+                                        SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy HH-mm-ss z");
+                                        String formattedDate = sdf.format(date);
+                                        times = times + ", " + formattedDate;
+                                        System.out.println(String.valueOf(stopEventArrival.getTime()));
+                                    }
                                 }
                             }
+                            timesView.setText(times);
                         }
                         catch(Exception e){
                             System.out.println("error");
+                            e.printStackTrace();
                         }
                     }
                 }
