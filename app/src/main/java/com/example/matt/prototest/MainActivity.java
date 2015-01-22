5128f0e1f6c1e06cb5eef7eb5bbe07412d49175f
Matt F <matthewf662@gmail.com>
2015-01-21 21:00:12 -0500

    Started to create protobuf fetch service to download from internet, working on getting result from service back

--- app/src/main/java/com/example/matt/prototest/MainActivity.java
+++ app/src/main/java/com/example/matt/prototest/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.matt.prototest;
 
+import android.content.Intent;
 import android.content.res.AssetManager;
+import android.os.Handler;
+import android.os.Parcelable;
+import android.os.ResultReceiver;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -14,6 +18,7 @@ import com.google.transit.realtime.GtfsRealtime.*;
 import com.google.protobuf.*;
 
 import java.io.*;
+import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLConnection;
 import java.text.DateFormat;
@@ -23,6 +28,8 @@ import java.util.TimeZone;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    PBReceiver resultPBReceiver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,38 +42,23 @@ public class MainActivity extends ActionBarActivity {
         GObutton.setOnClickListener(
                 new Button.OnClickListener() {
                     public void onClick(View v) {
-                        try {
-                            //URL url = new URL("http://opendata.hamilton.ca/GTFS-RT/GTFS_TripUpdates.pb");
-                            //URLConnection urlConnection = url.openConnection();
-                            //System.out.println("Opened Connection");
-                            //InputStream is = new BufferedInputStream(url.openStream());
-                            //System.out.println("Created url");
-                            AssetManager assetManager = getAssets();
-                            InputStream is = assetManager.open("GTFS_TripUpdates.pb");
-                            FeedMessage realdata = FeedMessage.parseFrom(is);
-                            System.out.println("parsed data");
-                            String times="";
-                            for (FeedEntity entity : realdata.getEntityList()){
-                                if(!entity.hasTripUpdate())continue;
-                                TripUpdate trip = entity.getTripUpdate();
-                                if (!trip.hasTrip())continue;
-                                for(TripUpdate.StopTimeUpdate stopTime : trip.getStopTimeUpdateList() ) {
-                                    if (stopTime.getStopId().equals(stopIDEdit.getText().toString())) {
-                                        TripUpdate.StopTimeEvent stopEventArrival = stopTime.getArrival();
-                                        long unixSeconds = stopEventArrival.getTime();
-                                        Date date = new Date(unixSeconds*1000);
-                                        SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy HH-mm-ss z");
-                                        String formattedDate = sdf.format(date);
-                                        times = times + ", " + formattedDate;
-                                        System.out.println(String.valueOf(stopEventArrival.getTime()));
-                                    }
+                        Intent fetchServiceIntent = new Intent(v.getContext(), PBFetchService.class);
+                        fetchServiceIntent.putExtra("reciever",resultPBReceiver);
+                        startService(fetchServiceIntent);
+                        for (FeedEntity entity : realdata.getEntityList()){
+                            if(!entity.hasTripUpdate())continue;
+                            TripUpdate trip = entity.getTripUpdate();
+                            if (!trip.hasTrip())continue;
+                            for(TripUpdate.StopTimeUpdate stopTime : trip.getStopTimeUpdateList() ) {
+                                if (stopTime.getStopId().equals(stopIDEdit.getText().toString())) {
+                                    TripUpdate.StopTimeEvent stopEventArrival = stopTime.getArrival();
+                                    long unixSeconds = stopEventArrival.getTime();
+                                    Date date = new Date(unixSeconds*1000);
+                                    SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy HH-mm-ss z");
+                                    String formattedDate = sdf.format(date);
+                                    System.out.println(String.valueOf(stopEventArrival.getTime()));
                                 }
                             }
-                            timesView.setText(times);
-                        }
-                        catch(Exception e){
-                            System.out.println("error");
-                            e.printStackTrace();
                         }
                     }
                 }
@@ -93,5 +85,15 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    class PBReceiver extends ResultReceiver {
+        public PBReceiver(Handler handler){
+            super(handler);
+        }
+        @Override
+        protected void onReceiveResult(int resultCode, Bundle resultData) {
+            // Handle response from IntentService here
+        }
     }
 }
