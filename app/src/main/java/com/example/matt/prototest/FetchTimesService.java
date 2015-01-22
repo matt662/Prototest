6ac32a06631fa3fc19ab8ff6f1bbe85e56df4010
Matt F <matthewf662@gmail.com>
2015-01-22 17:10:43 -0500

    Download stop times pb intent service is working, times are pulling live from internet. next step be able to put in stop code and link it to stop id

---
+++ app/src/main/java/com/example/matt/prototest/FetchTimesService.java
@@ -0,0 +1,73 @@
+package com.example.matt.prototest;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.content.Context;
+import android.os.Bundle;
+import android.os.ResultReceiver;
+
+import com.google.transit.realtime.GtfsRealtime.*;
+
+import java.io.BufferedInputStream;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * An {@link IntentService} subclass for handling asynchronous task requests in
+ * a service on a separate handler thread.
+ * <p/>
+ * helper methods.
+ */
+public class FetchTimesService extends IntentService {
+    /**
+     * Creates an IntentService.  Invoked by your subclass's constructor.
+     *
+     */
+    public static final int TIMES_UPDATE = 1000;
+    public FetchTimesService() {
+        super("FetchTimesService");
+    }
+    private String times="";
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        FeedMessage realData;
+        String stopId = intent.getStringExtra("stopID");
+        try {
+            URL url = new URL("http://opendata.hamilton.ca/GTFS-RT/GTFS_TripUpdates.pb");
+            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+            System.out.println("Opened Connection");
+            InputStream is = new BufferedInputStream(urlConnection.getInputStream());
+            System.out.println("Created input stream");
+            realData = FeedMessage.parseFrom(is);
+            System.out.println("parsed data");
+            is.close();
+            for (FeedEntity entity : realData.getEntityList()){
+                if(!entity.hasTripUpdate())continue;
+                TripUpdate trip = entity.getTripUpdate();
+                if (!trip.hasTrip())continue;
+                for(TripUpdate.StopTimeUpdate stopTime : trip.getStopTimeUpdateList() ) {
+                    if (stopTime.getStopId().equals(stopId)){
+                        TripUpdate.StopTimeEvent stopEventArrival = stopTime.getArrival();
+                        long unixSeconds = stopEventArrival.getTime();
+                        Date date = new Date(unixSeconds*1000);
+                        SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy HH-mm-ss z");
+                        String formattedDate = sdf.format(date);
+                        System.out.println(String.valueOf(stopEventArrival.getTime()));
+                        times = times + ", " + formattedDate;
+                    }
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("error");
+            e.printStackTrace();
+        }
+        ResultReceiver receiver = intent.getParcelableExtra("receiver");
+        Bundle resultData = new Bundle();
+        resultData.putString("stopTimes",times);
+        receiver.send(TIMES_UPDATE,resultData);
+    }
+}
--- app/src/main/java/com/example/matt/prototest/MainActivity.java
+++ app/src/main/java/com/example/matt/prototest/MainActivity.java
@@ -1,9 +1,7 @@
 package com.example.matt.prototest;
 
 import android.content.Intent;
-import android.content.res.AssetManager;
 import android.os.Handler;
-import android.os.Parcelable;
 import android.os.ResultReceiver;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
@@ -14,22 +12,8 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-import com.google.transit.realtime.GtfsRealtime.*;
-import com.google.protobuf.*;
-
-import java.io.*;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.net.URLConnection;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.TimeZone;
-
 
 public class MainActivity extends ActionBarActivity {
-
-    PBReceiver resultPBReceiver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,29 +21,15 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
         Button GObutton = (Button)findViewById(R.id.goButton);
         final EditText stopIDEdit = (EditText)findViewById(R.id.editText);
-        final TextView timesView = (TextView)findViewById(R.id.stopTimesText);
 
         GObutton.setOnClickListener(
                 new Button.OnClickListener() {
                     public void onClick(View v) {
-                        Intent fetchServiceIntent = new Intent(v.getContext(), PBFetchService.class);
-                        fetchServiceIntent.putExtra("reciever",resultPBReceiver);
-                        startService(fetchServiceIntent);
-                        for (FeedEntity entity : realdata.getEntityList()){
-                            if(!entity.hasTripUpdate())continue;
-                            TripUpdate trip = entity.getTripUpdate();
-                            if (!trip.hasTrip())continue;
-                            for(TripUpdate.StopTimeUpdate stopTime : trip.getStopTimeUpdateList() ) {
-                                if (stopTime.getStopId().equals(stopIDEdit.getText().toString())) {
-                                    TripUpdate.StopTimeEvent stopEventArrival = stopTime.getArrival();
-                                    long unixSeconds = stopEventArrival.getTime();
-                                    Date date = new Date(unixSeconds*1000);
-                                    SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy HH-mm-ss z");
-                                    String formattedDate = sdf.format(date);
-                                    System.out.println(String.valueOf(stopEventArrival.getTime()));
-                                }
-                            }
-                        }
+                        Intent fetchTimesIntent = new Intent(v.getContext(), FetchTimesService.class);
+                        fetchTimesIntent.putExtra("receiver",new TimesReciever(new Handler()));
+                        fetchTimesIntent.putExtra("stopID",stopIDEdit.getText().toString());
+                        startService(fetchTimesIntent);
+
                     }
                 }
         );    }
@@ -87,13 +57,19 @@ public class MainActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    class PBReceiver extends ResultReceiver {
-        public PBReceiver(Handler handler){
+    class TimesReciever extends ResultReceiver {
+        public TimesReciever(Handler handler){
             super(handler);
         }
         @Override
         protected void onReceiveResult(int resultCode, Bundle resultData) {
-            // Handle response from IntentService here
+            super.onReceiveResult(resultCode,resultData);
+            final TextView timesView = (TextView)findViewById(R.id.stopTimesText);
+            String times;
+            if (resultCode == FetchTimesService.TIMES_UPDATE){
+                times = resultData.getString("stopTimes");
+                timesView.setText(times);
+            }
         }
     }
 }
